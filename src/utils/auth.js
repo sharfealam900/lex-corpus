f3f53cac6706b339c586f4015c779524449a6002
sharfealam900 <sharfealam900@gmail.com>
2026-08-03 20:15:02 +0530

    vercel

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/v1/auth";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/auth";
 
 // =========================
 // Email Validation
