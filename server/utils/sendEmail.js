a7f9f87d70e36841e5c214afdfa16e4d5814dfd5
sharfealam900 <sharfealam900@gmail.com>
2026-08-04 22:49:25 +0530

    Use Gmail SMTP 587

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,20 +1,25 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendEmail = async (to, subject, html) => {
-    await transporter.sendMail({
-        from: `"Lex Corp" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
-    });
+  return transporter.sendMail({
+    from: `"Lex Corp" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
 };
 
 export default sendEmail;
